ee589efd64db5d04b256c43f43ff5b6384115c4f
Søren Skovsbøll <sorens@hey.com>
2022-02-20 14:10:07 +0100

    working interpolators

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,8 @@ import Axis
 import Browser exposing (Document)
 import Browser.Events
 import Color exposing (Color)
+import Html exposing (a, button, text)
+import Html.Events exposing (onClick)
 import Interpolation exposing (Interpolator)
 import Path exposing (Path)
 import Random
@@ -23,23 +25,32 @@ type Msg
     = Tick Int
     | DataRequested Time.Posix
     | DataArrived (List Series)
+    | OverlayClicked
+    | StackedClicked
+    | StreamClicked
+    | SeparateClicked
 
 
 type alias Extent a =
     ( a, a )
 
 
+type Configuration
+    = Overlaid
+    | Stacked
+    | Stream
+    | Separate
+
+
 type alias Model =
-    { series : List Series
+    { config : Configuration
+    , series : List Series
+    , seriesT : Transition (List Series)
     , xExtent : Extent Time.Posix
     , xExtentT : Transition (Extent Time.Posix)
     , yExtent : Extent Float
     , yExtentT : Transition (Extent Float)
     }
-
-
-type alias Datum =
-    { x : Time.Posix, y : Float, id : Int }
 
 
 type alias Series =
@@ -49,13 +60,26 @@ type alias Series =
     }
 
 
+type alias Datum =
+    { x : Time.Posix
+    , y0 : Float
+    , y1 : Float
+    , id : Int
+    }
+
+
 init : () -> ( Model, Cmd msg )
 init _ =
-    ( { series =
+    let
+        noDataSeries =
             [ { name = "Chrysler", data = [], color = Color.green }
             , { name = "Toyota", data = [], color = Color.blue }
             , { name = "Volvo", data = [], color = Color.red }
             ]
+    in
+    ( { config = Overlaid
+      , series = noDataSeries
+      , seriesT = Transition.constant noDataSeries
       , xExtent = initXExtent
       , xExtentT = Transition.constant initXExtent
       , yExtent = initYExtent
@@ -121,15 +145,15 @@ yAxis model =
 
 
 transformToLineData : Model -> Datum -> Maybe ( Float, Float )
-transformToLineData model { x, y } =
-    Just ( Scale.convert (xScale model) x, Scale.convert (yScale model) y )
+transformToLineData model { x, y1 } =
+    Just ( Scale.convert (xScale model) x, Scale.convert (yScale model) y1 )
 
 
 tranfromToAreaData : Model -> Datum -> Maybe ( ( Float, Float ), ( Float, Float ) )
-tranfromToAreaData model { x, y } =
+tranfromToAreaData model { x, y0, y1 } =
     Just
-        ( ( Scale.convert (xScale model) x, Tuple.first (Scale.rangeExtent (yScale model)) )
-        , ( Scale.convert (xScale model) x, Scale.convert (yScale model) y )
+        ( ( Scale.convert (xScale model) x, Scale.convert (yScale model) y0 )
+        , ( Scale.convert (xScale model) x, Scale.convert (yScale model) y1 )
         )
 
 
@@ -154,7 +178,19 @@ withOpacity opacity color =
     { c | alpha = opacity } |> Color.fromHsla
 
 
-view : Model -> Document msg
+viewSeries : Model -> List (Svg Msg)
+viewSeries model =
+    model.series
+        |> List.map
+            (\series ->
+                g []
+                    [ Path.element (area series model) [ strokeWidth 3, fill <| Paint <| withOpacity 0.5 <| series.color ]
+                    , Path.element (line series model) [ stroke <| Paint <| series.color, strokeWidth 3, fill PaintNone ]
+                    ]
+            )
+
+
+view : Model -> Document Msg
 view model =
     { title = ""
     , body =
@@ -164,16 +200,12 @@ view model =
             , g [ transform [ Translate (padding - 1) padding ] ]
                 [ yAxis model ]
             , g [ transform [ Translate padding padding ], class [ "series" ] ]
-                (model.series
-                    |> List.map
-                        (\series ->
-                            g []
-                                [ Path.element (area series model) [ strokeWidth 3, fill <| Paint <| withOpacity 0.5 <| series.color ]
-                                , Path.element (line series model) [ stroke <| Paint <| series.color, strokeWidth 3, fill PaintNone ]
-                                ]
-                        )
-                )
+                (viewSeries model)
             ]
+        , button [ onClick OverlayClicked ] [ text "Overlaid" ]
+        , button [ onClick StackedClicked ] [ text "Stacked" ]
+        , button [ onClick StreamClicked ] [ text "Stream" ]
+        , button [ onClick SeparateClicked ] [ text "Separate" ]
         ]
     }
 
@@ -186,6 +218,20 @@ span model =
             Tuple.mapBoth Time.posixToMillis Time.posixToMillis model.xExtent
     in
     Tuple.second pair - Tuple.first pair
+
+
+stackOffsetSeparated : List (List ( Float, Float )) -> List (List ( Float, Float ))
+stackOffsetSeparated series =
+    List.foldl
+        (\s1 ( maxSoFar, accum ) ->
+            ( maxSoFar + (s1 |> List.map Tuple.second |> List.maximum |> Maybe.withDefault 0)
+            , List.map (\( _, hi ) -> ( maxSoFar, maxSoFar + hi )) s1 :: accum
+            )
+        )
+        ( 0, [] )
+        series
+        |> Tuple.second
+        |> List.reverse
 
 
 interpolatePosix : Time.Posix -> Time.Posix -> Interpolator Time.Posix
@@ -203,6 +249,31 @@ interpolatePosix from to =
 interpolateExtent : Extent Time.Posix -> Extent Time.Posix -> Interpolator (Extent Time.Posix)
 interpolateExtent =
     Interpolation.tuple interpolatePosix interpolatePosix
+
+
+interpolateSeriesList : List Series -> List Series -> Interpolator (List Series)
+interpolateSeriesList a b f =
+    List.map2 (\sa sb -> interpolateSeries sa sb f) a b
+
+
+interpolateSeries : Series -> Series -> Interpolator Series
+interpolateSeries a b f =
+    { a | data = interpolateData a.data b.data f }
+
+
+interpolateData : List Datum -> List Datum -> Interpolator (List Datum)
+interpolateData a b f =
+    List.map2 (\da db -> interpolateDatum da db f) a b
+
+
+interpolateDatum : Datum -> Datum -> Interpolator Datum
+interpolateDatum a b =
+    Interpolation.map4 Datum (interpolatePosix a.x b.x) (Interpolation.float a.y0 b.y0) (Interpolation.float a.y1 b.y1) (constant a.id b.id)
+
+
+constant : Int -> Int -> Interpolator Int
+constant a _ =
+    always a
 
 
 days : Float -> Int
@@ -250,7 +321,7 @@ update msg model =
                     newSeries
                         |> List.map .data
                         |> List.concat
-                        |> List.map .y
+                        |> List.map .y1
                         |> Statistics.extent
                         |> Maybe.withDefault ( 0.0, 5.0 )
                         |> Tuple.mapFirst (always 0.0)
@@ -274,6 +345,58 @@ update msg model =
             , Cmd.none
             )
 
+        OverlayClicked ->
+            ( { model
+                | config = Overlaid
+                , seriesT = Transition.for 500 (interpolateSeriesList (Transition.value model.seriesT) (overlaid model.series))
+              }
+            , Cmd.none
+            )
+
+        StackedClicked ->
+            ( { model
+                | config = Stacked
+                , seriesT = Transition.for 500 (interpolateSeriesList (Transition.value model.seriesT) (stacked model.series))
+              }
+            , Cmd.none
+            )
+
+        StreamClicked ->
+            ( { model
+                | config = Stream
+                , seriesT = Transition.for 500 (interpolateSeriesList (Transition.value model.seriesT) (stream model.series))
+              }
+            , Cmd.none
+            )
+
+        SeparateClicked ->
+            ( { model
+                | config = Separate
+                , seriesT = Transition.for 500 (interpolateSeriesList (Transition.value model.seriesT) (separate model.series))
+              }
+            , Cmd.none
+            )
+
+
+overlaid : List Series -> List Series
+overlaid series =
+    series
+
+
+stacked : List Series -> List Series
+stacked series =
+    series
+
+
+stream : List Series -> List Series
+stream series =
+    series
+
+
+separate : List Series -> List Series
+separate series =
+    series
+
 
 main : Program () Model Msg
 main =
@@ -285,15 +408,15 @@ main =
         }
 
 
-sequence : List (Random.Generator a) -> Random.Generator (List a)
-sequence =
+randomSequence : List (Random.Generator a) -> Random.Generator (List a)
+randomSequence =
     List.foldr (Random.map2 (::)) (Random.constant [])
 
 
 randomSeriess : List Series -> Time.Posix -> Random.Generator (List Series)
 randomSeriess series startTime =
     Random.int 3 20
-        |> Random.andThen (\length -> List.map (\s -> randomSeries length s startTime) series |> sequence)
+        |> Random.andThen (\length -> List.map (\s -> randomSeries length s startTime) series |> randomSequence)
 
 
 randomSeries : Int -> Series -> Time.Posix -> Random.Generator Series
@@ -316,7 +439,7 @@ randomData length startTime =
                     newId |> Time.millisToPosix
             in
             if length_ > 1 then
-                Random.map2 (::) (Random.map3 Datum (Random.constant newStart) (Random.float 200.0 350.0) (Random.constant newId)) (randomDatum (length_ - 1) newStart list)
+                Random.map2 (::) (Random.map4 Datum (Random.constant newStart) (Random.constant 0.0) (Random.float 200.0 350.0) (Random.constant newId)) (randomDatum (length_ - 1) newStart list)
 
             else
                 list
